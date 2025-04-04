8ec323a876130fa3a2470240fc3e0997c067c488
SpicyRamen <94422507+itotoostressed@users.noreply.github.com>
2025-04-04 14:44:00 -0400

    Added player attack & attack detection

--- player.js
+++ player.js
@@ -7,17 +7,17 @@ const FRICTION = 0.9;
 const GRAVITY = -0.7;
 const JUMP_FORCE = 15;
 const MAX_JUMP = 2;
-var onPlatform = false;
 
 class GameObject {
     constructor(left, bottom, width, height) {
         this.left = left;
-        this.bottom = bottom
+        this.bottom = bottom;
         this.width = width;
         this.height = height;
         this.top = bottom + height;
         this.right = left + width;
         this.element = null;
+        this.onPlatform = false;
     }
 
     intToPx(num) {
@@ -52,16 +52,35 @@ class Player extends GameObject {
         super(left, bottom, width, height);
         this.element = document.getElementById("box");
         this.velocity = [0, 0];
+        this.ground = 0;
         this.numJumps = 0;
         this.isJumping = false;
         this.isCrouching = false;
-        this.ground = 0;
+        this.isAttacking = false;
+        this.attackCooldown = 0;
+        this.attackRange = 100;
         this.keysPressed = {
             left: false,
             right: false,
             up: false,
             down: false
         };
+    }
+
+    attack() {
+        if (this.attackCooldown <= 0) {
+            this.isAttacking = true;
+            this.attackCooldown = 30;
+            
+            // Visual feedback
+            this.element.style.boxShadow = "0 0 15px yellow";
+            setTimeout(() => {
+                this.element.style.boxShadow = "0px 4px 10px #202020";
+            }, 100);
+            
+            return true;
+        }
+        return false;
     }
 
     updatePosition(platforms) {
@@ -82,6 +101,12 @@ class Player extends GameObject {
         // Apply friction
         if (!this.keysPressed.left && !this.keysPressed.right) {
             this.velocity[X] *= FRICTION;
+        }
+
+        if (this.attackCooldown > 0) {
+            this.attackCooldown--;
+        } else {
+            this.isAttacking = false;
         }
 
         // Limit velocity
@@ -121,9 +146,9 @@ class Player extends GameObject {
         platforms.forEach(platform => {
             if (this.bottom >= platform.top && this.right > platform.left && this.left < platform.right) {
                 this.ground = platform.top;
-                onPlatform = true;
+                this.onPlatform = true;
             }
-            if (this.keysPressed.down && onPlatform) {
+            if (this.keysPressed.down && this.onPlatform) {
                 this.ground = 0;
             }
         });
@@ -147,6 +172,11 @@ class Player extends GameObject {
                 break;
             case "s":
                 this.keysPressed.down = true;
+                break;
+            case "e":
+                if (this.attack()) {
+                    game.checkAttackHit(this.keysPressed.left ? -1 : 1);
+                }
                 break;
         }
     }
@@ -218,6 +248,7 @@ class Camera {
         this.follow = followObject;
         this.container = container;
         this.offsetX = 0;
+        this.offsetY = 0;
         this.updateScreenBounds();
         window.addEventListener("resize", () => this.updateScreenBounds());
     }
@@ -228,12 +259,19 @@ class Camera {
         } else if (this.follow.left < this.screenFollowLeft - this.offsetX) {
             this.offsetX = this.follow.left - this.screenFollowLeft;
         }
-        this.container.style.transform = `translateX(${-this.offsetX}px)`;
+        if (this.follow.bottom > this.screenFollowUp - this.offsetY) {
+            this.offsetY = this.follow.bottom - this.screenFollowUp;
+        } else if (this.follow.bottom < this.screenFollowDown - this.offsetY) {
+            this.offsetY = this.follow.bottom - this.screenFollowDown;
+        }
+        this.container.style.transform = `translate(${-this.offsetX}px, ${this.offsetY}px)`;
     }
 
     updateScreenBounds() {
         this.screenFollowRight = window.innerWidth * 0.75;
         this.screenFollowLeft = window.innerWidth * 0.25;
+        this.screenFollowUp = window.innerHeight * 0.75;
+        this.screenFollowDown = window.innerHeight * 0.25;
     }
 }
 
@@ -244,7 +282,7 @@ class Game {
         this.enemies = [];
         this.player = null;
         this.camera = null;
-        this.worldWidth = 5000;
+        this.worldWidth = 3000;
         this.initialize();
     }
 
@@ -258,11 +296,25 @@ class Game {
             new Platform(400, 200, 600, 20),
             new Platform(2000, 200, 600, 20),
             new Platform(5000, 200, 600, 20),
-            new Platform(400, 400, 600, 20)
+            new Platform(400, 400, 600, 20),
+            new Platform(400, 600, 600, 20),
+            new Platform(400, 800, 600, 20),
+            new Platform(400, 1000, 600, 20),
+            new Platform(400, 1200, 600, 20),
+            new Platform(400, 1400, 600, 20),
+            new Platform(400, 1600, 600, 20),
+            new Platform(400, 1800, 600, 20),
+            new Platform(400, 2000, 600, 20),
+            new Platform(400, 2200, 600, 20),
+            new Platform(400, 2400, 600, 20),
+            new Platform(400, 2600, 600, 20),
+            new Platform(400, 2800, 600, 20),
+            new Platform(400, 3000, 600, 20),
+            new Platform(400, 3200, 600, 20),
         ];
 
         // Create enemies
-        this.enemies = [new Enemy(1000, 500, 80, 160)];
+        this.enemies = [new Enemy(this.platforms[1].left, this.platforms[1].top, 80, 160)];
 
         // Initialize camera
         this.camera = new Camera(this.player, this.gameContainer);
@@ -275,14 +327,54 @@ class Game {
         this.gameLoop();
     }
 
+    checkAttackHit(direction) {
+        const attackRange = this.player.attackRange;
+        const attackHitbox = {
+            left: direction > 0 ? this.player.right : this.player.left - attackRange,
+            right: direction > 0 ? this.player.right + attackRange : this.player.left,
+            bottom: this.player.bottom,
+            top: this.player.top
+        };
+
+        // Visual feedback
+        this.showAttackRange(attackHitbox);
+
+        // Check enemy collisions
+        this.enemies.forEach(enemy => {
+            if (this.checkHitboxCollision(attackHitbox, enemy)) {
+                enemy.element.style.backgroundColor = "purple";
+                setTimeout(() => {
+                    enemy.element.style.backgroundColor = "red";
+                }, 200);
+            }
+        });
+    }
+
+    showAttackRange(hitbox) {
+        const originalStyle = this.player.element.style.cssText;
+        this.player.element.style.width = `${hitbox.right - hitbox.left}px`;
+        this.player.element.style.left = `${hitbox.left - this.camera.offsetX}px`;
+        this.player.element.style.opacity = "0.7";
+        setTimeout(() => {
+            this.player.element.style.cssText = originalStyle;
+        }, 100);
+    }
+
+    checkHitboxCollision(hitbox, enemy) {
+        return (
+            hitbox.left < enemy.right &&
+            hitbox.right > enemy.left &&
+            hitbox.bottom < enemy.top &&
+            hitbox.top > enemy.bottom
+        );
+    }
+
     gameLoop() {
         this.player.updatePosition(this.platforms);
-        
         this.enemies.forEach(enemy => {
             enemy.move(this.worldWidth);
             this.checkPlayerEnemyCollision(enemy);
         });
-
         this.camera.update();
         requestAnimationFrame(() => this.gameLoop());
     }
@@ -295,5 +387,8 @@ class Game {
     }
 }
 
-// Start the game when the page loads
-window.onload = () => new Game();
+// Start the game
+let game;
+window.onload = () => {
+    game = new Game();
+};
